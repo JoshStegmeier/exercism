41055d219cd94637e9f59e4b765d9e3011b0f98d
Josh Stegmeier <josh@steg.me>
2016-04-11 15:18:30 -0600

    RNA-Transcription (fetched)

---
+++ javascript/rna-transcription/rna-transcription.js
@@ -0,0 +1,25 @@
+var DnaTranscriber = function() {};
+
+DnaTranscriber.prototype.toRna = function(input) {
+
+    console.log(input);
+
+    // var num = 0;
+    // var i = 0;
+    //
+    // if (a.length !== b.length) {
+    //     throw news Error ("DNA strands must be of equal length.");
+    // }
+    //
+    // while (i < a.length) {
+    //     if (a.charAt(i) !== b.charAt(i)) {
+    //         num ++;
+    //     }
+    //     i ++;
+    // }
+    //
+    // return num;
+
+};
+
+module.exports = DnaTranscriber;
