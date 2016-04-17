2651bad33a4c6b9dd6ac9f3fed2422e65b82ecda
Josh Stegmeier <josh@steg.me>
2016-04-17 00:15:12 -0600

    Notes and experiments before submitting

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -6,9 +6,38 @@
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-//
-// YOUR CODE GOES HERE
-//
+
+    var a = input;
+
+    var notBlank = new RegExp(/\S/g);
+    var notBlankTest = notBlank.test(a);
+
+    var hasLetters= new RegExp(/[a-z]/i);
+    var hasLettersTest = hasLetters.test(a);
+
+    if (a === a.toUpperCase() && hasLettersTest === true) {
+        return "Whoa, chill out!";
+    } else if (a.substr(-1) === "?") {
+        return "Sure.";
+    } else if (/\S/g.test(a) === false) {
+        return "Fine. Be that way!";
+    } else {
+        return "Whatever.";
+    }
+
+    // var b = a.match("\\?", "g");
+    // var c = a.match(/\S/g);
+    //
+    //     if ( b === true ) {
+    //         return "Sure.";
+    //     } else if (a === a.toUpperCase()) {
+    //         return "Whoa, chill out!";
+    //     } else if (c === true) {
+    //         return "Fine. Be that way!";
+    //     } else {
+    //         return "Whatever.";
+    //     }
+
 };
 
 module.exports = Bob;
