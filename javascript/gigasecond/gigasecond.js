b09bc2ef7cfe7426aa2df456d151047651ee5adf
Josh Stegmeier <josh@steg.me>
2016-04-17 20:27:14 -0600

    Successful! Commit before v1 submission to Exercism.io

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,20 +1,33 @@
 var Gigasecond = function(input) {
-//
-// YOUR CODE GOES HERE
-//
+    startDate = input;
+};
 
-var inputtedDate = (input);
+Gigasecond.prototype.date = function() {
 
-console.log(input);
-console.log(inputtedDate);
-var gigasecondVal = 1000000000000;
-var returnedDate = new Date(inputtedDate.getTime() + gigasecondVal);
-console.log(returnedDate);
-return returnedDate;
-
-
-
+    var gigasecondVal = 1000000000000;
+    var returnedDate = new Date(startDate.getTime() + gigasecondVal);
+    return returnedDate;
 
 };
 
 module.exports = Gigasecond;
+
+
+// var Gigasecond = function(input) {
+// //
+// // YOUR CODE GOES HERE
+// //
+//
+// var inputtedDate = (input);
+//
+// console.log(input);
+// console.log(inputtedDate);
+// var gigasecondVal = 1000000000000;
+// var returnedDate = new Date(inputtedDate.getTime() + gigasecondVal);
+// console.log(returnedDate);
+//
+// return returnedDate;
+//
+// };
+//
+// module.exports = Gigasecond;
