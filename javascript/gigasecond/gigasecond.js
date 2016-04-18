76f3266bf647e63b7eef61c86e3870db064c4972
Josh Stegmeier <josh@steg.me>
2016-04-17 20:28:06 -0600

    Whitespace removed.

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -3,11 +3,9 @@ var Gigasecond = function(input) {
 };
 
 Gigasecond.prototype.date = function() {
-
     var gigasecondVal = 1000000000000;
     var returnedDate = new Date(startDate.getTime() + gigasecondVal);
     return returnedDate;
-
 };
 
 module.exports = Gigasecond;
