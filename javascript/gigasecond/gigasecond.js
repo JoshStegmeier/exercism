58c0aef8f42d66b05d57b5176fd9cf4f04b65288
Josh Stegmeier <josh@steg.me>
2016-04-17 20:39:08 -0600

    Removed comments from gigasecond.js. Fetched word-count

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -9,23 +9,3 @@ Gigasecond.prototype.date = function() {
 };
 
 module.exports = Gigasecond;
-
-
-// var Gigasecond = function(input) {
-// //
-// // YOUR CODE GOES HERE
-// //
-//
-// var inputtedDate = (input);
-//
-// console.log(input);
-// console.log(inputtedDate);
-// var gigasecondVal = 1000000000000;
-// var returnedDate = new Date(inputtedDate.getTime() + gigasecondVal);
-// console.log(returnedDate);
-//
-// return returnedDate;
-//
-// };
-//
-// module.exports = Gigasecond;
